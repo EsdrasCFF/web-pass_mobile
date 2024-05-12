cceeeb7a83dba8ce778325eb30680199e8e8d8f1
EsdrasCFF <ecff.dev@gmail.com>
2024-05-12 12:54:36 -0300

    feat: add font theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,16 @@ import "./global.css"
 import { useFonts, Roboto_700Bold, Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto'
 
 export default function Layout() {
+  const [ fontsLoaded ] = useFonts({
+    Roboto_700Bold, 
+    Roboto_500Medium, 
+    Roboto_400Regular
+  })
+
+  if(!fontsLoaded) {
+    return
+  }
+
   return  (
     <>
       <StatusBar style="light" />
