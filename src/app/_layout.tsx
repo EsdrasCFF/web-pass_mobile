9a412bb1087f354ca7222d2d155029f1746eee59
EsdrasCFF <ecff.dev@gmail.com>
2024-05-12 12:54:22 -0300

    feat: add font theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,8 @@ import { StatusBar } from "expo-status-bar";
 // Import your global CSS file
 import "./global.css"
 
+import { useFonts, Roboto_700Bold, Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto'
+
 export default function Layout() {
   return  (
     <>
