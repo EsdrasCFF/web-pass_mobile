9935be1ac4849865075a4a5093fc83c28e48879d
EsdrasCFF <ecff.dev@gmail.com>
2024-05-12 17:09:23 -0300

    feat: add token of the colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,14 @@
+import { fontFamily } from './src/assets/styles/fonts'
+import { colors } from './src/assets/styles/colors'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      fontFamily: {
-        regular: 'Roboto_400Regular',
-        bold: 'Roboto_700Bold',
-        medium: 'Roboto_500Medium',
-      },
-      colors: {
-        gray: {
-          100: '#E1E1E6',
-          200: '#C4C4CC',
-          300: '#8D8D99',
-        },
-        green: {
-          200: '#9FF9CC',
-          400: '#28494E',
-          500: '#00292E',
-        },
-        orange: {
-          500: '#F48F56',
-        },
-      },
+      fontFamily,
+      colors,
     },
   },
   plugins: [],
