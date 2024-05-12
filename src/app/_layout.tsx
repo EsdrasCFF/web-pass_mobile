4e4a88dca8cff196f3e44ae4f332e86d06fa9bf5
EsdrasCFF <ecff.dev@gmail.com>
2024-05-12 13:05:14 -0300

    feat: use loading component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,9 +2,10 @@ import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 // Import your global CSS file
-import "./global.css"
+import "@/app/global.css"
 
 import { useFonts, Roboto_700Bold, Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto'
+import { Loading } from "@/components/loading";
 
 export default function Layout() {
   const [ fontsLoaded ] = useFonts({
@@ -13,14 +14,11 @@ export default function Layout() {
     Roboto_400Regular
   })
 
-  if(!fontsLoaded) {
-    return
-  }
 
   return  (
     <>
       <StatusBar style="light" />
-      <Slot />
+      { fontsLoaded ? <Slot /> : <Loading /> }
     </>
   )
   }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function Home() {
   return ( 
     <View className='bg-red-400  flex-1 items-center justify-center'>
-      <Text className='text-white' >Hello React Native</Text>
+      <Text className='text-white font-bold text-2xl' >Hello React Native</Text>
     </View>
   )
 }
